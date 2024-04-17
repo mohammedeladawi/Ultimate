ddb1b4e36aaf231bf5cff4f129496339169a4342
mohammedeladawi <mohammedeladwe3@gmail.com>
2024-04-17 14:40:38 +0400

    Added functionality to the event section

--- js/main.js
+++ js/main.js
@@ -26,9 +26,40 @@ const progressTagIncreaser = (percentTag, goal) => {
   let countUp = setInterval(() => {
     let percentValue = parseInt(percentTag.textContent);
     percentTag.textContent = `${percentValue + 1}%`;
-    console.log(percentTag.textContent);
     if (percentValue == goal) clearInterval(countUp);
   }, 500 / goal);
 };
 
 window.addEventListener("scroll", handleSkillsOnScroll);
+
+// Events functionality
+const daySpan = document.querySelector(".events .time .days");
+const hourSpan = document.querySelector(".events .time .hours");
+const minuteSpan = document.querySelector(".events .time .minutes");
+const secondSpan = document.querySelector(".events .time .seconds");
+
+function updateCountdown() {
+  const goalDate = new Date("Dec 31, 2024 23:59:59").getTime();
+  const nowDate = new Date().getTime();
+  const diffDate = goalDate - nowDate;
+
+  let days = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+  daySpan.textContent = days < 10 ? `0${days}` : days;
+
+  let hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  hourSpan.textContent = hours < 10 ? `0${hours}` : hours;
+
+  let minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+  minuteSpan.textContent = minutes < 10 ? `0${minutes}` : minutes;
+
+  let seconds = Math.floor((diffDate % (1000 * 60)) / 1000);
+  secondSpan.textContent = seconds < 10 ? `0${seconds}` : seconds;
+
+  // Stop the counter when it reaches this time
+  if (diffDate <= 0) clearInterval(eventCountDown);
+}
+
+const eventCountDown = setInterval(updateCountdown, 1000);
+
+// Display them immediately
+updateCountdown();
